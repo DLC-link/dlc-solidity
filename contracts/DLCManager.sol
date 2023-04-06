4d909d6f2c78c2804d287856ca2fe8c681694ed2
scolear <bobtera@gmail.com>
2023-04-06 11:57:49 +0200

    sepolia redeploys

--- contracts/DLCManager.sol
+++ contracts/DLCManager.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.8.17;
 
 // import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./DLCLinkCompatible.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './DLCLinkCompatible.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 
 contract DLCManager is AccessControl {
-    bytes32 public constant DLC_ADMIN_ROLE = keccak256("DLC_ADMIN_ROLE");
+    bytes32 public constant DLC_ADMIN_ROLE = keccak256('DLC_ADMIN_ROLE');
     bytes32[] public openUUIDs;
     uint256 private _localNonce = 0;
     address public btcPriceFeedAddress;
@@ -64,7 +64,7 @@ contract DLCManager is AccessControl {
         // We cap ERT in about 3110 years just to be safe
         require(
             _emergencyRefundTime < 99999999999,
-            "Emergency Refund Time is too high"
+            'Emergency Refund Time is too high'
         );
 
         bytes32 _uuid = _generateUUID(tx.origin, ++_localNonce);
@@ -74,7 +74,7 @@ contract DLCManager is AccessControl {
             tx.origin,
             _emergencyRefundTime,
             _nonce,
-            "dlclink:create-dlc:v0"
+            'dlclink:create-dlc:v0'
         );
         return _uuid;
     }
@@ -111,7 +111,7 @@ contract DLCManager is AccessControl {
             _receiver,
             _emergencyRefundTime,
             _nonce,
-            "dlclink:post-create-dlc:v0"
+            'dlclink:post-create-dlc:v0'
         );
     }
 
@@ -119,7 +119,7 @@ contract DLCManager is AccessControl {
 
     function setStatusFunded(bytes32 _uuid) external onlyRole(DLC_ADMIN_ROLE) {
         DLCLinkCompatible(dlcs[_uuid].creator).setStatusFunded(_uuid);
-        emit SetStatusFunded(_uuid, "dlclink:set-status-funded:v0");
+        emit SetStatusFunded(_uuid, 'dlclink:set-status-funded:v0');
     }
 
     event MintBtcNft(
@@ -132,13 +132,13 @@ contract DLCManager is AccessControl {
 
     function mintBtcNft(bytes32 _uuid, uint256 _collateral) external {
         _findIndex(_uuid); // Reverts if DLC is not in the list of open DLCs
-        DLC memory _dlc = dlcs[_uuid];
+        DLC storage _dlc = dlcs[_uuid];
         emit MintBtcNft(
             _uuid,
             _dlc.creator,
             _dlc.receiver,
             _collateral,
-            "dlclink:mint-btc-nft:v0"
+            'dlclink:mint-btc-nft:v0'
         );
     }
 
@@ -149,7 +149,7 @@ contract DLCManager is AccessControl {
         uint256 _nftId
     ) external onlyRole(DLC_ADMIN_ROLE) {
         DLCLinkCompatible(dlcs[_uuid].creator).postMintBtcNft(_uuid, _nftId);
-        emit PostMintBtcNft(_uuid, _nftId, "dlclink:post-mint-btc-nft:v0");
+        emit PostMintBtcNft(_uuid, _nftId, 'dlclink:post-mint-btc-nft:v0');
     }
 
     event CloseDLC(
@@ -162,13 +162,13 @@ contract DLCManager is AccessControl {
     function closeDLC(bytes32 _uuid, uint256 _outcome) public {
         // Access control?
         DLC storage _dlc = dlcs[_uuid];
-        require(_dlc.uuid != 0, "Unknown DLC");
+        require(_dlc.uuid != 0, 'Unknown DLC');
         _dlc.outcome = _outcome; // Saving requested outcome
         emit CloseDLC(
             _uuid,
             _outcome,
             dlcs[_uuid].creator,
-            "dlclink:close-dlc:v0"
+            'dlclink:close-dlc:v0'
         );
     }
 
@@ -184,8 +184,8 @@ contract DLCManager is AccessControl {
         uint256 _oracleOutcome
     ) external onlyRole(DLC_ADMIN_ROLE) {
         DLC storage _dlc = dlcs[_uuid];
-        require(_dlc.uuid != 0, "Unknown DLC");
-        require(_dlc.outcome == _oracleOutcome, "Different Outcomes");
+        require(_dlc.uuid != 0, 'Unknown DLC');
+        require(_dlc.outcome == _oracleOutcome, 'Different Outcomes');
 
         _removeClosedDLC(_findIndex(_uuid));
         DLCLinkCompatible(_dlc.creator).postCloseDLCHandler(_uuid);
@@ -193,7 +193,7 @@ contract DLCManager is AccessControl {
             _uuid,
             _oracleOutcome,
             block.timestamp,
-            "dlclink:post-close-dlc:v0"
+            'dlclink:post-close-dlc:v0'
         );
     }
 
@@ -217,7 +217,7 @@ contract DLCManager is AccessControl {
             _uuid,
             msg.sender,
             price,
-            "dlc-link:get-btc-price-with-callback:v0"
+            'dlc-link:get-btc-price-with-callback:v0'
         );
         return price;
     }
@@ -253,7 +253,7 @@ contract DLCManager is AccessControl {
                 return i;
             }
         }
-        revert("DLC Not Found");
+        revert('DLC Not Found');
     }
 
     function getAllUUIDs() public view returns (bytes32[] memory) {
