528b5399005780f5dcd9f331c9f712e3fbc09bb9
scolear <bobtera@gmail.com>
2023-07-19 12:36:27 +0200

    feat: LendingContractV1

---
+++ contracts/examples/LendingDemo/LendingDemoV1.sol
@@ -0,0 +1,328 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.17;
+
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
+import '../../DLCManagerV1.sol';
+import '../../DLCLinkCompatibleV1.sol';
+import 'hardhat/console.sol';
+
+enum LoanStatus {
+    None,
+    Ready,
+    Funded,
+    PreRepaid,
+    Repaid,
+    PreLiquidated,
+    Liquidated
+}
+
+struct Loan {
+    uint256 id;
+    bytes32 dlcUUID;
+    string[] attestorList;
+    LoanStatus status;
+    uint256 vaultLoan; // the borrowed amount
+    uint256 vaultCollateral; // btc deposit in sats
+    uint256 liquidationRatio; // the collateral/loan ratio below which liquidation can happen, with two decimals precision (140% = u14000)
+    uint256 liquidationFee; // additional fee taken during liquidation, two decimals precision (10% = u1000)
+    address owner; // the account owning this loan
+}
+
+contract LendingContractV1 is DLCLinkCompatibleV1, AccessControl {
+    using SafeMath for uint256;
+    DLCManagerV1 private _dlcManager;
+    IERC20 private _usdc;
+    address private _btcPriceFeedAddress;
+    address private _protocolWalletAddress;
+
+    bytes32 public constant ADMIN_ROLE = keccak256('ADMIN_ROLE');
+    bytes32 public constant DLC_MANAGER_ROLE = keccak256('DLC_MANAGER_ROLE');
+    uint256 public index = 0;
+    mapping(uint256 => Loan) public loans;
+    mapping(bytes32 => uint256) public loanIDsByUUID;
+    mapping(address => uint256) public loansPerAddress;
+
+    constructor(
+        address _dlcManagerAddress,
+        address _usdcAddress,
+        address _protocolWallet,
+        address _priceFeedAddress
+    ) {
+        _dlcManager = DLCManagerV1(_dlcManagerAddress);
+        _usdc = IERC20(_usdcAddress);
+        _protocolWalletAddress = _protocolWallet;
+        _btcPriceFeedAddress = _priceFeedAddress;
+        _setupRole(ADMIN_ROLE, _msgSender());
+        _setupRole(DLC_MANAGER_ROLE, _dlcManagerAddress);
+    }
+
+    modifier onlyAdmin() {
+        require(
+            hasRole(ADMIN_ROLE, _msgSender()),
+            'LendingContract: must have admin role to perform this action'
+        );
+        _;
+    }
+
+    modifier onlyDLCManager() {
+        require(
+            hasRole(DLC_MANAGER_ROLE, _msgSender()),
+            'LendingContract: must have dlc-manager role to perform this action'
+        );
+        _;
+    }
+
+    event SetupLoan(
+        bytes32 dlcUUID,
+        uint256 btcDeposit,
+        uint256 liquidationRatio,
+        uint256 liquidationFee,
+        uint256 index,
+        string[] attestorList,
+        address owner
+    );
+
+    function setupLoan(
+        uint256 btcDeposit,
+        uint256 liquidationRatio,
+        uint256 liquidationFee,
+        uint8 attestorCount
+    ) external returns (uint256) {
+        (bytes32 _uuid, string[] memory attestorList) = _dlcManager.createDLC(
+            _protocolWalletAddress,
+            attestorCount
+        );
+
+        loans[index] = Loan({
+            id: index,
+            dlcUUID: _uuid,
+            attestorList: attestorList,
+            status: LoanStatus.Ready,
+            vaultLoan: 0,
+            vaultCollateral: btcDeposit,
+            liquidationRatio: liquidationRatio,
+            liquidationFee: liquidationFee,
+            owner: msg.sender
+        });
+
+        loanIDsByUUID[_uuid] = index;
+
+        emit SetupLoan(
+            _uuid,
+            btcDeposit,
+            liquidationRatio,
+            liquidationFee,
+            index,
+            attestorList,
+            msg.sender
+        );
+
+        emit StatusUpdate(index, _uuid, LoanStatus.Ready);
+
+        loansPerAddress[msg.sender]++;
+        index++;
+
+        return (index - 1);
+    }
+
+    event StatusUpdate(uint256 loanid, bytes32 dlcUUID, LoanStatus newStatus);
+
+    function _updateStatus(uint256 _loanID, LoanStatus _status) internal {
+        Loan storage _loan = loans[_loanID];
+        require(_loan.status != _status, 'Status already set');
+        _loan.status = _status;
+        require(_loan.status == _status, 'Failed to set status');
+        emit StatusUpdate(_loanID, _loan.dlcUUID, _status);
+    }
+
+    function setStatusFunded(bytes32 _uuid) external override onlyDLCManager {
+        require(loans[loanIDsByUUID[_uuid]].dlcUUID != 0, 'No such loan');
+        _updateStatus(loanIDsByUUID[_uuid], LoanStatus.Funded);
+    }
+
+    event BorrowEvent(
+        uint256 loanid,
+        bytes32 dlcUUID,
+        uint256 amount,
+        uint256 vaultLoan,
+        LoanStatus status
+    );
+
+    function borrow(uint256 _loanID, uint256 _amount) public {
+        Loan storage _loan = loans[_loanID];
+        require(_loan.owner == msg.sender, 'Unathorized');
+        require(_loan.status == LoanStatus.Funded, 'Loan not funded');
+        // TODO:
+        //  - user shouldnt be able to overborrow (based on collateral value)
+        // require(_loan.vaultLoan ... )
+        _usdc.transfer(_loan.owner, _amount);
+        _loan.vaultLoan = _loan.vaultLoan.add(_amount);
+        emit BorrowEvent(
+            _loanID,
+            _loan.dlcUUID,
+            _amount,
+            _loan.vaultLoan,
+            _loan.status
+        );
+    }
+
+    event RepayEvent(
+        uint256 loanid,
+        bytes32 dlcUUID,
+        uint256 amount,
+        uint256 vaultLoan,
+        LoanStatus status
+    );
+
+    function repay(uint256 _loanID, uint256 _amount) public {
+        Loan storage _loan = loans[_loanID];
+        require(_loan.owner == msg.sender, 'Unathorized');
+        require(_loan.vaultLoan >= _amount, 'Amount too large');
+        _usdc.transferFrom(_loan.owner, address(this), _amount);
+        _loan.vaultLoan = _loan.vaultLoan.sub(_amount);
+        emit RepayEvent(
+            _loanID,
+            _loan.dlcUUID,
+            _amount,
+            _loan.vaultLoan,
+            _loan.status
+        );
+    }
+
+    function closeLoan(uint256 _loanID) public {
+        Loan memory _loan = loans[_loanID];
+        require(_loan.owner == msg.sender, 'Unathorized');
+        require(_loan.vaultLoan == 0, 'Loan not repaid');
+        _updateStatus(_loanID, LoanStatus.PreRepaid);
+        _dlcManager.closeDLC(_loan.dlcUUID, 0);
+    }
+
+    function postCloseDLCHandler(bytes32 _uuid) external onlyDLCManager {
+        Loan memory _loan = loans[loanIDsByUUID[_uuid]];
+        require(_loan.dlcUUID != 0, 'No such loan');
+        require(
+            _loan.status == LoanStatus.PreRepaid ||
+                _loan.status == LoanStatus.PreLiquidated,
+            'Invalid Loan Status'
+        );
+        _updateStatus(
+            _loan.id,
+            _loan.status == LoanStatus.PreRepaid
+                ? LoanStatus.Repaid
+                : LoanStatus.Liquidated
+        );
+    }
+
+    // Gives back BTC price data. 8 decimals, e.g. $22,836 = 2283600000000
+    function _getLatestPrice(
+        address _feedAddress
+    ) internal view returns (int256, uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(_feedAddress);
+        (, int256 price, , uint256 timeStamp, ) = priceFeed.latestRoundData();
+        return (price, timeStamp);
+    }
+
+    event DoesNotNeedLiquidation(
+        uint256 loanid,
+        bytes32 dlcUUID,
+        LoanStatus status
+    );
+
+    function attemptLiquidate(uint256 _loanID) public {
+        (int256 _price, uint256 _timestamp) = _getLatestPrice(
+            _btcPriceFeedAddress
+        );
+        Loan memory _loan = loans[_loanID];
+        bool _needsLiquidation = checkLiquidation(_loan.id, _price);
+        if (!_needsLiquidation) {
+            emit DoesNotNeedLiquidation(_loan.id, _loan.dlcUUID, _loan.status);
+            return;
+        }
+        uint16 _payoutRatio = calculatePayoutRatio(_loan.id, _price);
+        _liquidateLoan(_loan.id, _payoutRatio);
+    }
+
+    function _liquidateLoan(uint256 _loanID, uint16 _payoutRatio) internal {
+        _updateStatus(_loanID, LoanStatus.PreLiquidated);
+        _dlcManager.closeDLC(loans[_loanID].dlcUUID, _payoutRatio);
+    }
+
+    function checkLiquidation(
+        uint256 _loanID,
+        int256 _price
+    ) public view returns (bool) {
+        // if liquidationRatio is 14000 (140.00%)
+        // and collateralvalue is 2968680000000
+        // and price is 2283600000000
+        // and vaultLoan is 2000 USDLC -- 20000000000000000000 (16 decimals)
+        // We need to check if the collateral/loan ratio is below liquidationRatio%
+
+        uint256 _collateralValue = getCollateralValue(_loanID, _price); // 8 decimals
+        uint256 _strikePrice = SafeMath.div(
+            SafeMath.mul(
+                loans[_loanID].vaultLoan,
+                loans[_loanID].liquidationRatio
+            ),
+            10 ** 14
+        );
+
+        return _collateralValue <= _strikePrice;
+    }
+
+    function calculatePayoutRatio(
+        uint256 _loanID,
+        int256 _price
+    ) public view returns (uint16) {
+        // Should return a number between 0-100.00 (0-10000)
+        Loan memory _loan = loans[_loanID];
+        uint256 _collateralValue = getCollateralValue(_loanID, _price); // 8 decimals
+        uint256 _sellToLiquidatorsRatio = SafeMath.div(
+            _loan.vaultLoan,
+            _collateralValue
+        );
+        uint256 _payoutRatioPrecise = _sellToLiquidatorsRatio +
+            SafeMath.mul(_sellToLiquidatorsRatio, _loan.liquidationFee);
+        uint256 _payoutRatio = SafeMath.div(_payoutRatioPrecise, 10 ** 9);
+
+        return _payoutRatio >= 10000 ? 10000 : uint16(_payoutRatio);
+    }
+
+    function getCollateralValue(
+        uint256 _loanID,
+        int256 _price
+    ) public view returns (uint256) {
+        //  _price is 8 decimals, e.g. $22,836 = 2283600000000
+        // If collateral is 1.3 BTC, stored as 130000000 sats
+        // 130000000 * 2283600000000 = 2.9E20
+        // we divide by 10**8 to get 2968680000000
+        return
+            SafeMath.div(
+                (loans[_loanID].vaultCollateral * uint256(_price)),
+                10 ** 8
+            );
+    }
+
+    function getLoan(uint256 _loanID) public view returns (Loan memory) {
+        return loans[_loanID];
+    }
+
+    function getLoanByUUID(bytes32 _uuid) public view returns (Loan memory) {
+        return loans[loanIDsByUUID[_uuid]];
+    }
+
+    function getAllLoansForAddress(
+        address _addy
+    ) public view returns (Loan[] memory) {
+        Loan[] memory ownedLoans = new Loan[](loansPerAddress[_addy]);
+        uint256 j = 0;
+        for (uint256 i = 0; i < index; i++) {
+            if (loans[i].owner == _addy) {
+                ownedLoans[j++] = loans[i];
+            }
+        }
+        return ownedLoans;
+    }
+}
--- scripts/10_deploy-V1.js
+++ scripts/10_deploy-V1.js
@@ -6,10 +6,12 @@ const {
     deploymentInfo,
     loadDeploymentInfo,
 } = require('./helpers/deployment-handlers_versioned');
+const chainlinkPricefeedAddresses = require('./helpers/chainlink-pricefeed-addresses');
 
 module.exports = async function deployV1(attestorCount) {
     await hardhat.run('compile');
     const network = hardhat.network.name;
+    let CLpricefeed = chainlinkPricefeedAddresses[network];
     const accounts = await hardhat.ethers.getSigners();
     const admin = accounts[0];
     const protocol = accounts[1];
@@ -61,5 +63,62 @@ module.exports = async function deployV1(attestorCount) {
     await saveDeploymentInfo(
         deploymentInfo(hardhat, mockProtocol, 'MockProtocol'),
         'v1'
+    );
+
+    if (network === 'localhost') {
+        console.log(
+            `deploying contract MockV3Aggregator to network "${network}"...`
+        );
+        const MockV3Aggregator = await hardhat.ethers.getContractFactory(
+            'MockV3Aggregator'
+        );
+        const mockV3Aggregator = await MockV3Aggregator.deploy(0, 0);
+        await mockV3Aggregator.deployTransaction.wait();
+        console.log(
+            `deployed contract MockV3Aggregator to ${mockV3Aggregator.address} (network: ${network})`
+        );
+        CLpricefeed = mockV3Aggregator.address;
+    }
+
+    /////////////// Lending Demo ///////////////
+    // USDC contract deployment
+    console.log(
+        `deploying contract for token USDStableCoinForDLCs (USDC) to network "${network}"...`
+    );
+    const USDC = await hardhat.ethers.getContractFactory(
+        'USDStableCoinForDLCs'
+    );
+    const usdc = await USDC.deploy();
+    await usdc.deployed();
+    console.log(
+        `deployed contract for token USDStableCoinForDLCs (USDC) to ${usdc.address} (network: ${network})`
+    );
+    await saveDeploymentInfo(deploymentInfo(hardhat, usdc, 'USDC'), 'v1');
+
+    console.log(
+        `deploying contract LendingContractV1 to network "${network}"...`
+    );
+    const LendingDemo = await hardhat.ethers.getContractFactory(
+        'LendingContractV1'
+    );
+    const lendingDemo = await LendingDemo.deploy(
+        dlcManager.address,
+        usdc.address,
+        protocolWallet.address,
+        CLpricefeed
+    );
+
+    await lendingDemo.deployed();
+    console.log(
+        `deployed contract LendingContractV1 to ${lendingDemo.address} (network: ${network})`
+    );
+    await saveDeploymentInfo(
+        deploymentInfo(hardhat, lendingDemo, 'LendingContract'),
+        'v1'
+    );
+
+    await usdc.mint(
+        lendingDemo.address,
+        hardhat.ethers.utils.parseUnits('10000000', 'ether')
     );
 };
