acfd55e9b20fbfed3627699c85b4db0ec3adc93f
Robert Tera <37669800+scolear@users.noreply.github.com>
2024-04-10 13:34:27 +0200

    feat: set tss commitment to timestamp (#53)

--- scripts/08-set-tss-commitment.js
+++ scripts/08-set-tss-commitment.js
@@ -2,26 +2,66 @@ const {
     callManagerContractFunction,
 } = require('./helpers/00-call-dlc-manager-fn');
 const { ethers } = require('ethers');
+const hardhat = require('hardhat');
+const prompts = require('prompts');
+const {
+    loadDeploymentInfo,
+} = require('./helpers/deployment-handlers_versioned');
 
-module.exports = async function setTSSCommitment(secretIdentifier, version) {
-    let secretIdentifierBytes;
-    if (secretIdentifier == null) {
-        secretIdentifierBytes = ethers.utils.randomBytes(32);
-    } else {
-        secretIdentifierBytes = ethers.utils.toUtf8Bytes(secretIdentifier);
-    }
-
-    console.log(
-        'Setting TSS commitment with secret identifier: ',
-        secretIdentifier
+module.exports = async function setTSSCommitment(version) {
+    const accounts = await hardhat.ethers.getSigners();
+    const admin = accounts[0];
+    const deployInfo = await loadDeploymentInfo(
+        hardhat.network.name,
+        'DLCManager',
+        version
+    );
+    const contract = new hardhat.ethers.Contract(
+        deployInfo.contract.address,
+        deployInfo.contract.abi,
+        admin
     );
 
-    const commitment = ethers.utils.keccak256(secretIdentifierBytes);
+    const currentCommitmentBytes32 = await contract.tssCommitment();
+    console.log('Current Commitment: ', currentCommitmentBytes32);
+    const currentCommitment = ethers.utils.parseBytes32String(
+        currentCommitmentBytes32
+    );
+    console.log('Current Commitment (string): ', currentCommitment);
+
+    const response = await prompts({
+        type: 'select',
+        name: 'set-unset',
+        message: 'Do you want to set or unset the TSS commitment?',
+        choices: [
+            { title: 'Set (to Timestamp)', value: 'set' },
+            { title: 'Unset (to HashZero)', value: 'unset' },
+        ],
+    });
+
+    let commitment, commitmentBytes32;
+
+    if (response['set-unset'] === 'unset') {
+        commitment = ethers.constants.HashZero;
+        commitmentBytes32 = ethers.constants.HashZero;
+    } else if (response['set-unset'] === 'set') {
+        // lets make the commitment a UNIX timestamp in seconds
+        commitment = Math.floor(Date.now() / 1000);
+        // Convert the number to a string and then to bytes32
+        commitmentBytes32 = ethers.utils.formatBytes32String(
+            commitment.toString()
+        );
+    } else {
+        console.log('No action taken');
+        return;
+    }
+
     console.log('Commitment: ', commitment.toString());
+    console.log('Commitment (bytes32): ', commitmentBytes32);
 
     await callManagerContractFunction(
         'setTSSCommitment',
-        [commitment],
+        [commitmentBytes32],
         version
     );
 };
