a500de753958b7ec1b07ce2b447103b962fae52e
scolear <bobtera@gmail.com>
2024-06-05 12:49:37 +0200

    feat: set deposit limits script

---
+++ scripts/16_set-deposit-limit.js
@@ -0,0 +1,23 @@
+const {
+    callTokenManagerFunction,
+} = require('./helpers/10-call-token-manager-fn');
+
+async function setDepositLimit(minOrMax, newLimit) {
+    let functionToCall;
+    if (minOrMax === 'min') {
+        functionToCall = 'setMinimumDeposit';
+    } else if (minOrMax === 'max') {
+        functionToCall = 'setMaximumDeposit';
+    } else {
+        throw new Error('Invalid minOrMax argument');
+    }
+    await callTokenManagerFunction(functionToCall, [newLimit]);
+}
+
+module.exports = setDepositLimit;
+
+if (require.main === module) {
+    const minOrMax = process.argv[2];
+    const newLimit = process.argv[3];
+    setDepositLimit(minOrMax, newLimit).catch(console.error);
+}
