27c38075a79987ba03848e51eb7128b01514614d
scolear <bobtera@gmail.com>
2024-08-26 17:55:31 +0200

    wip: new chain quick setup script

---
+++ scripts/helpers/new-chain-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Mainnet attestor public keys
+dlc-link-eth add-signer 0x989E9c4005ABc2a8E4b85544B44d2d95cfDe08de # DLC.Link
+dlc-link-eth add-signer 0xBe4aAE47A62f67bdF93eA9f5F189ae51B1b54492 # DLC.Link
+dlc-link-eth add-signer 0x7B254D8C6eBd9662A52180B06920aEA4f23a8940 # DLC.Link
+dlc-link-eth add-signer 0x2daef70747eb9E97E5f31A9EBDbda593918F8bE7 # Stakin
+dlc-link-eth add-signer 0x2b16469227cd34F591D455aC81Ca8a1A4bA69F02 # LinkPool
+dlc-link-eth add-signer 0x5db792d5facb35e9adce8a151e8a3d0d36c9cb77 # HashKey
+dlc-link-eth add-signer 0x194c697e8343EaB3C53917BA7e597d02687f8BA0 # Despread
+dlc-link-eth add-signer 0xdf4d8B54dE476B674f1832B95984fFa7e223d47B # PierTwo
+
+dlc-link-eth set-attestor-gpk 'xpub6C1F2SwADP3TNajQjg2PaniEGpZLvWdMiFP8ChPjQBRWD1XUBeMdE4YkQYvnNhAYGoZKfcQbsRCefserB5DyJM7R9VR6ce6vLrXHVfeqyH3'
+
+dlc-link-eth set-btc-fee-recipient 021b34f36d8487ce3a7a6f0124f58854d561cb52077593d1e86973fac0fea1a8b1
