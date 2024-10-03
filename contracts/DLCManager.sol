88812a96bd8e7da519e10cdafef4fa421662ce8c
scolear <bobtera@gmail.com>
2024-10-03 18:19:08 +0200

    wip: enzyme-forks

--- contracts/DLCManager.sol
+++ contracts/DLCManager.sol
@@ -358,6 +358,7 @@ contract DLCManager is
         return _uuid;
     }
 
+    // TODO: auth
     function setupPendingVault(
         bytes32 _uuid,
         string calldata _taprootPubKey,
--- contracts/PoolMerchant.sol
+++ contracts/PoolMerchant.sol
@@ -11,8 +11,47 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlDefaultAdminRule
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./DLCLinkLibrary.sol";
 import "./DLCManager.sol";
+
+// interface IEnzymeVault {
+//     function buyShares(
+//         uint256 _investmentAmount,
+//         uint256 _minSharesQuantity
+//     ) external returns (uint256);
+// }
+
+interface IGlobalConfigLibComptrollerV4 {
+    function buyShares(
+        uint256 _investmentAmount,
+        uint256 _minSharesQuantity
+    ) external returns (uint256 sharesReceived_);
+
+    function getDenominationAsset()
+        external
+        view
+        returns (address denominationAsset_);
+
+    function redeemSharesForSpecificAssets(
+        address _recipient,
+        uint256 _sharesQuantity,
+        address[] calldata _payoutAssets,
+        uint256[] calldata _payoutAssetPercentages
+    ) external returns (uint256[] memory payoutAmounts_);
+
+    function redeemSharesInKind(
+        address _recipient,
+        uint256 _sharesQuantity,
+        address[] calldata _additionalAssets,
+        address[] calldata _assetsToSkip
+    )
+        external
+        returns (
+            address[] memory payoutAssets_,
+            uint256[] memory payoutAmounts_
+        );
+}
 
 contract PoolMerchant is
     Initializable,
@@ -21,22 +60,31 @@ contract PoolMerchant is
 {
     using DLCLink for DLCLink.DLC;
     using Strings for string;
+    using SafeERC20 for IERC20;
 
     bytes32 public constant DLC_ADMIN_ROLE =
         0x2bf88000669ee6f7a648a231f4adbc117f5a8e34f980c08420b9b9a9f2640aa1; // keccak256("DLC_ADMIN_ROLE")
 
+    IGlobalConfigLibComptrollerV4 public enzymeVault;
     DLCManager public dlcManager;
+    IERC20 public dlcBTC;
     uint256 private _nonce;
     mapping(bytes32 => string) public uuidToTaprootPubkey;
+    mapping(bytes32 => address) public uuidToUserAddress;
+    mapping(bytes32 => uint256) public sweptAmounts;
 
     function initialize(
         address defaultAdmin,
         address dlcAdminRole,
-        DLCManager dlcManagerContract
+        address dlcManagerContract,
+        address dlcBTCContract,
+        address enzymeVaultContract
     ) public initializer {
         __AccessControlDefaultAdminRules_init(2 days, defaultAdmin);
         _grantRole(DLC_ADMIN_ROLE, dlcAdminRole);
-        dlcManager = dlcManagerContract;
+        dlcManager = DLCManager(dlcManagerContract);
+        dlcBTC = IERC20(dlcBTCContract);
+        enzymeVault = IGlobalConfigLibComptrollerV4(enzymeVaultContract);
         _nonce = 0;
     }
 
@@ -52,7 +100,9 @@ contract PoolMerchant is
     }
 
     // TODO: auth
+    // called by attestors
     function createPendingVault(
+        address userAddress,
         bytes32 uuid,
         string memory taprootPubkey,
         string calldata wdTxId
@@ -61,6 +111,7 @@ contract PoolMerchant is
             revert VaultAlreadyExists(uuid);
         }
         uuidToTaprootPubkey[uuid] = taprootPubkey;
+        uuidToUserAddress[uuid] = userAddress;
         dlcManager.setupPendingVault(uuid, taprootPubkey, wdTxId);
     }
 
@@ -70,7 +121,46 @@ contract PoolMerchant is
         return shares;
     }
 
-    // sweepDeposit() {
-    //  loop vaults and perform deposit for vaults where there is new shares
-    //}
+    function getSweptAmountForUUID(bytes32 uuid) public view returns (uint256) {
+        return sweptAmounts[uuid];
+    }
+
+    // called by attestors
+    function sweepDeposit() public {
+        DLCLink.DLC[] memory _allDLCs = dlcManager.getAllVaultsForAddress(
+            address(this)
+        );
+
+        for (uint256 i = 0; i < _allDLCs.length; i++) {
+            bytes32 uuid = _allDLCs[i].uuid;
+            uint256 currentValueMinted = _allDLCs[i].valueMinted;
+            uint256 sweptAmount = sweptAmounts[uuid];
+
+            if (currentValueMinted > sweptAmount) {
+                uint256 difference = currentValueMinted - sweptAmount;
+
+                // Approve the Enzyme vault to spend dlcBTC tokens
+                dlcBTC.approve(address(enzymeVault), difference);
+
+                // Simulate the call to buyShares to get the _minSharesQuantity
+                (bool success, bytes memory result) = address(enzymeVault)
+                    .staticcall(
+                        abi.encodeWithSignature(
+                            "buyShares(uint256,uint256)",
+                            difference,
+                            1
+                        )
+                    );
+
+                require(success, "Static call to buyShares failed");
+
+                uint256 minSharesQuantity = abi.decode(result, (uint256));
+
+                // Actual call to buyShares with the obtained minSharesQuantity
+                enzymeVault.buyShares(difference, minSharesQuantity);
+
+                sweptAmounts[uuid] = currentValueMinted; // Update the local tracker
+            }
+        }
+    }
 }
