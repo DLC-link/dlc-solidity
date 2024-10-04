a52f0897ef9556513418a1f3dac6467127d84f16
scolear <bobtera@gmail.com>
2024-10-04 17:24:45 +0200

    wip: working deposit flow, early withdraw

--- scripts/enzyme-tests.js
+++ scripts/enzyme-tests.js
@@ -28,6 +28,9 @@ async function main() {
         '0x0DD4f29E21F10cb2E485cf9bDAb9F2dD1f240Bfa'
     );
 
+    const enzymeComptrollerAddress =
+        '0x0c164A2708477C8930E8964b3B8EB9038C86Ffee';
+    const enzymeVaultAddress = '0xe1f20e9855b2bce29f92579ba193230389795b46';
     const admin = accounts[0];
     const userAddress = accounts[1].address;
 
@@ -51,37 +54,66 @@ async function main() {
         deployInfoToken.contract.abi,
         admin
     );
+    let dlcManager;
 
-    const DLCManager = await hardhat.ethers.getContractFactory('DLCManager');
-    const dlcManager = await hardhat.upgrades.deployProxy(DLCManager, [
-        '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
-        '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
-        2,
-        dlcBTC.address,
-        btcFeeRecipient,
-    ]);
-    await dlcManager.deployed();
-    await setSigners(dlcManager, attestors);
+    const dlcManagerExists = await hardhat.ethers.getContractAt(
+        'DLCManager',
+        '0xe4097Ee7b1AE8a7E5DdCaD820Ca31ee4d2d9Ef92',
+        admin
+    );
 
-    const txTranferOwnership = await dlcManagerExisting
-        .connect(dlcAdminSafe)
-        .transferTokenContractOwnership(dlcManager.address);
-    await txTranferOwnership.wait();
+    // console.log('dlcManagerExists:', dlcManagerExists);
+
+    // TODO: this check doesnt really work
+    if (process.env.FORCE_DEPLOY) {
+        const DLCManager =
+            await hardhat.ethers.getContractFactory('DLCManager');
+        dlcManager = await hardhat.upgrades.deployProxy(DLCManager, [
+            '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+            '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+            2,
+            dlcBTC.address,
+            btcFeeRecipient,
+        ]);
+        await dlcManager.deployed();
+        await setSigners(dlcManager, attestors);
+
+        const txTranferOwnership = await dlcManagerExisting
+            .connect(dlcAdminSafe)
+            .transferTokenContractOwnership(dlcManager.address);
+        await txTranferOwnership.wait();
+    } else {
+        dlcManager = dlcManagerExists;
+    }
 
     console.log('dlcManager:', dlcManager.address);
     console.log('dlcBTC:', dlcBTC.address);
     console.log('dlcBTC owner:', await dlcBTC.owner());
 
-    const PoolMerchant =
-        await hardhat.ethers.getContractFactory('PoolMerchant');
-    const poolMerchant = await hardhat.upgrades.deployProxy(PoolMerchant, [
-        '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
-        '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
-        dlcManager.address,
-        dlcBTC.address,
-        '0x0c164A2708477C8930E8964b3B8EB9038C86Ffee',
-    ]);
-    await poolMerchant.deployed();
+    let poolMerchant;
+
+    const pmExists = await hardhat.ethers.getContractAt(
+        'PoolMerchant',
+        '0xf96C190E181b38c840B7832BbA9E8D527250a5FB',
+        admin
+    );
+
+    // console.log('pmExists:', pmExists);
+
+    if (process.env.FORCE_DEPLOY) {
+        const PoolMerchant =
+            await hardhat.ethers.getContractFactory('PoolMerchant');
+        poolMerchant = await hardhat.upgrades.deployProxy(PoolMerchant, [
+            '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+            '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+            dlcManager.address,
+            dlcBTC.address,
+            enzymeComptrollerAddress,
+        ]);
+        await poolMerchant.deployed();
+    } else {
+        poolMerchant = pmExists;
+    }
 
     const pm = new hardhat.ethers.Contract(
         poolMerchant.address,
@@ -95,7 +127,7 @@ async function main() {
     console.log('uuid:', uuid);
 
     const enzymeVault = new hardhat.ethers.Contract(
-        '0x0c164A2708477C8930E8964b3B8EB9038C86Ffee',
+        enzymeComptrollerAddress,
         [
             'function buyShares(uint256, uint256) external returns (uint256)',
             'function getDenominationAsset() external view returns (address)',
@@ -104,7 +136,7 @@ async function main() {
         admin
     );
     const asset = await enzymeVault.getDenominationAsset();
-    console.log('asset:', asset);
+    console.log('asset on enzyme:', asset);
 
     await enzymeVault
         .connect(enzymeManager)
@@ -124,15 +156,20 @@ async function main() {
     const fakeTapPK = 'fakeTapPK';
     const deposit = 100000000;
 
-    await dlcManager.connect(admin).whitelistAddress(pm.address);
-    console.log('whitelisted pm');
+    try {
+        await dlcManager.connect(admin).whitelistAddress(pm.address);
+        console.log('whitelisted pm on dlcManager');
+    } catch (error) {
+        console.error('error whitelisting pm on dlcManager:', error);
+    }
     await pm
         .connect(admin)
         .createPendingVault(userAddress, uuid, fakeTapPK, fakeWdTxId);
-    console.log('created pending vault');
+    console.log('created pending vault through pm');
     const dlc = await dlcManager.getDLC(uuid);
-    console.log('dlc:', dlc);
+    // console.log('dlc:', dlc);
     const pmBalanceBefore = await dlcBTC.balanceOf(pm.address);
+    console.log('pmBalanceBefore Funded:', pmBalanceBefore.toString());
 
     const signatureBytesForFunding = await getSignatures(
         {
@@ -144,33 +181,106 @@ async function main() {
         attestors,
         3
     );
-    const tx3 = await dlcManager
+    const tx2 = await dlcManager
         .connect(attestor1)
         .setStatusFunded(uuid, fakeWdTxId, signatureBytesForFunding, deposit);
-    await tx3.wait();
+    await tx2.wait();
 
     const pmBalanceAfter = await dlcBTC.balanceOf(pm.address);
-    console.log('pmBalanceBefore:', pmBalanceBefore.toString());
-    console.log('pmBalanceAfter:', pmBalanceAfter.toString());
+    console.log('pmBalanceAfter Funded:', pmBalanceAfter.toString());
 
     console.log(
         'pmSweptBalanceForUUIDBefore:',
-        await pm.getSweptAmountForUUID(uuid)
+        (await pm.getSweptAmountForUUID(uuid)).toString()
     );
 
+    const enzymeVaultBalanceBefore = await dlcBTC.balanceOf(enzymeVaultAddress);
+    console.log(
+        'enzymeVaultBalanceBefore:',
+        enzymeVaultBalanceBefore.toString()
+    );
+
+    console.log('sweeping deposits...');
     const txSweep = await pm.connect(admin).sweepDeposit();
-    await txSweep.wait();
+    const txReceipt = await txSweep.wait();
+
+    // console.log('txEvents:', txReceipt.events);
 
     console.log(
         'pmSweptBalanceForUUIDAfter:',
-        await pm.getSweptAmountForUUID(uuid)
+        (await pm.getSweptAmountForUUID(uuid)).toString()
     );
     const pmBalanceAfterSweep = await dlcBTC.balanceOf(pm.address);
     console.log('pmBalanceAfterSweep:', pmBalanceAfterSweep.toString());
-    const enzymeBalance = await dlcBTC.balanceOf(
-        '0x0c164A2708477C8930E8964b3B8EB9038C86Ffee'
+    const enzymeVaultBalanceAfter = await dlcBTC.balanceOf(enzymeVaultAddress);
+    console.log('enzymeVaultBalanceAfter:', enzymeVaultBalanceAfter.toString());
+
+    console.log('withdrawing funds...');
+    const withdrawAmount = 50000;
+    const txWithdraw = await pm
+        .connect(admin)
+        .withdraw(uuid, withdrawAmount, fakeTapPK, fakeWdTxId);
+    const txReceiptWithdraw = await txWithdraw.wait();
+
+    const pmBalanceAfterWithdraw = await dlcBTC.balanceOf(pm.address);
+    console.log('pmBalanceAfterWithdraw:', pmBalanceAfterWithdraw.toString());
+    const enzymeVaultBalanceAfterWithdraw =
+        await dlcBTC.balanceOf(enzymeVaultAddress);
+    console.log(
+        'enzymeVaultBalanceAfterWithdraw:',
+        enzymeVaultBalanceAfterWithdraw.toString()
     );
-    console.log('enzymeBalance:', enzymeBalance.toString());
+    const sweptAmountsAfterWithdraw = await pm.getSweptAmountForUUID(uuid);
+    console.log(
+        'sweptAmountsAfterWithdraw:',
+        sweptAmountsAfterWithdraw.toString()
+    );
+
+    // withdraw funds
+    // set pending
+    // set funded with less amount
+    // sweepredeem
+    // check balances
+
+    // const signatureBytesForPending = await getSignatures(
+    //     {
+    //         uuid,
+    //         btcTxId: fakeWdTxId,
+    //         functionString: 'set-status-pending',
+    //         newLockedAmount: 0,
+    //     },
+    //     attestors,
+    //     3
+    // );
+    // const tx3 = await dlcManager
+    //     .connect(attestor1)
+    //     .setStatusPending(
+    //         uuid,
+    //         fakeWdTxId,
+    //         signatureBytesForPending,
+    //         fakeTapPK,
+    //         0
+    //     );
+    // await tx3.wait();
+    // const signatureBytesForFunding2 = await getSignatures(
+    //     {
+    //         uuid,
+    //         btcTxId: fakeWdTxId,
+    //         functionString: 'set-status-funded',
+    //         newLockedAmount: newAmountLocked,
+    //     },
+    //     attestors,
+    //     3
+    // );
+    // const tx4 = await dlcManager
+    //     .connect(attestor1)
+    //     .setStatusFunded(
+    //         uuid,
+    //         fakeWdTxId,
+    //         signatureBytesForFunding2,
+    //         newAmountLocked
+    //     );
+    // await tx4.wait();
 }
 
 main()
