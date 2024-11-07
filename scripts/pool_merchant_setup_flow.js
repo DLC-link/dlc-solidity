77f03436d2c4044be07535110123464d8835bfd6
scolear <bobtera@gmail.com>
2024-11-07 20:10:24 +0100

    wip: arbsep deployment

--- scripts/pool_merchant_setup_flow.js
+++ scripts/pool_merchant_setup_flow.js
@@ -1,12 +1,20 @@
+require('dotenv').config();
 const hre = require('hardhat');
 const { ethers, upgrades } = require('hardhat');
-const { loadContractAddress } = require('./helpers/utils');
+const { loadContractAddress, promptUser } = require('./helpers/utils');
 const {
     saveDeploymentInfo,
     deploymentInfo,
 } = require('./helpers/deployment-handlers_versioned');
 
 async function main() {
+    const network = hre.network.name;
+    const shouldContinue = await promptUser(
+        `You are about to interact with ${network}.\n Continue?`
+    );
+    if (!shouldContinue) {
+        throw new Error('Deployment aborted by user.');
+    }
     // Compile contracts
     console.log('\n🔨 Compiling contracts...');
     await hre.run('compile');
@@ -16,69 +24,81 @@ async function main() {
     const accounts = await ethers.getSigners();
     const deployer = accounts[0];
     const dlcAdmin = deployer;
-    const attestor_1 = ''; // TODO:
+    const attestor_1 = '0x3355977947F84C2b1CAE7D2903a72958aEE185e2'; // TODO: devnet attestor 1
     const operator = deployer;
     const harvester = deployer;
 
     // DLCManager upgrade remains the same
     console.log('\n🔄 Upgrading DLCManager...');
-    const network = hre.network.name;
     const proxyAddress = await loadContractAddress('DLCManager', network);
-    const newImplementation = await ethers.getContractFactory('DLCManager');
-    await upgrades.upgradeProxy(proxyAddress, newImplementation);
+    // const newImplementation = await ethers.getContractFactory('DLCManager');
+    // await upgrades.upgradeProxy(proxyAddress, newImplementation);
 
-    await hre.run('verify:verify', {
-        address: proxyAddress,
-    });
-    console.log('DLCManager upgraded');
+    // await hre.run('verify:verify', {
+    //     address: proxyAddress,
+    // });
+    // console.log('DLCManager upgraded');
 
     console.log('\n📝 Getting contract instances...');
     const dlcManager = await ethers.getContractAt('DLCManager', proxyAddress);
 
-    try {
-        await saveDeploymentInfo(
-            deploymentInfo(network, dlcManager, 'DLCManager')
-        );
-    } catch (error) {
-        console.error(error);
-    }
+    // try {
+    //     await saveDeploymentInfo(
+    //         deploymentInfo(network, dlcManager, 'DLCManager')
+    //     );
+    // } catch (error) {
+    //     console.error(error);
+    // }
 
     const dlcBTCAddress = await loadContractAddress('DLCBTC', network);
     const dlcBTC = await ethers.getContractAt('DLCBTC', dlcBTCAddress);
 
     // Deploy PoolMerchant
     console.log('\n🏗️ Deploying PoolMerchant...');
-    const PoolMerchant = await ethers.getContractFactory('PoolMerchant');
-    const poolMerchant = await upgrades.deployProxy(PoolMerchant, [
-        proxyAddress,
-        dlcBTCAddress,
-        deployer.address,
-    ]);
-    await poolMerchant.deployed();
-    console.log('PoolMerchant deployed to:', poolMerchant.address);
-    try {
-        await saveDeploymentInfo(
-            deploymentInfo(network, poolMerchant, 'PoolMerchant')
-        );
-    } catch (error) {
-        console.error(error);
-    }
-    await hre.run('verify:verify', {
-        address: poolMerchant.address,
-    });
+    const poolMerchantAddress = await loadContractAddress(
+        'PoolMerchant',
+        network
+    );
+    const poolMerchant = await ethers.getContractAt(
+        'PoolMerchant',
+        poolMerchantAddress
+    );
+    // const PoolMerchant = await ethers.getContractFactory('PoolMerchant');
+    // const poolMerchant = await upgrades.deployProxy(PoolMerchant, [
+    //     proxyAddress,
+    //     dlcBTCAddress,
+    //     deployer.address,
+    // ]);
+    // await poolMerchant.deployed();
+    // console.log('PoolMerchant deployed to:', poolMerchant.address);
+    // try {
+    //     await saveDeploymentInfo(
+    //         deploymentInfo(network, poolMerchant, 'PoolMerchant')
+    //     );
+    // } catch (error) {
+    //     console.error(error);
+    // }
+    // await hre.run('verify:verify', {
+    //     address: poolMerchant.address,
+    // });
 
-    // Whitelist PoolMerchant
-    await dlcManager.connect(dlcAdmin).whitelistAddress(poolMerchant.address);
+    // // Whitelist PoolMerchant
+    // await dlcManager.connect(dlcAdmin).whitelistAddress(poolMerchant.address);
 
     console.log('\n🏗️  Deploying MockERC4626Vault...');
     const MockERC4626Vault =
         await ethers.getContractFactory('MockERC4626Vault');
-    const mockVault = await MockERC4626Vault.deploy(dlcBTCAddress);
-    await mockVault.deployed();
-    console.log('MockERC4626Vault deployed to:', mockVault.address);
-    await hre.run('verify:verify', {
-        address: mockVault.address,
-    });
+
+    const mockVault = await ethers.getContractAt(
+        'MockERC4626Vault',
+        '0x7B4c1663945767426177277Fa04777912720D39A'
+    );
+    // const mockVault = await MockERC4626Vault.deploy(dlcBTCAddress);
+    // await mockVault.deployed();
+    // console.log('MockERC4626Vault deployed to:', mockVault.address);
+    // await hre.run('verify:verify', {
+    //     address: mockVault.address,
+    // });
 
     console.log('\n🏗️  Deploying IntegrationSample...');
     const IntegrationSample =
@@ -95,9 +115,14 @@ async function main() {
     );
     await integrationSample.deployed();
     console.log('IntegrationSample deployed to:', integrationSample.address);
-    await hre.run('verify:verify', {
-        address: integrationSample.address,
-    });
+
+    try {
+        await hre.run('verify:verify', {
+            address: integrationSample.address,
+        });
+    } catch (error) {
+        console.error(error);
+    }
 
     // Setup roles and integration
     console.log('\n🔑 Setting up roles and integration...');
