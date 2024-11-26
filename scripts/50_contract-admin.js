d6b94f6f49af66dba73645177ea8081712f64001
scolear <bobtera@gmail.com>
2024-11-26 16:22:39 +0100

    chore: upgrade base DLCManager

--- scripts/50_contract-admin.js
+++ scripts/50_contract-admin.js
@@ -410,7 +410,15 @@ module.exports = async function contractAdmin() {
                 timeLockContractDeployInfo.contract.abi,
                 deployer
             );
+
             const proxyAdmin = await hardhat.upgrades.admin.getInstance();
+            const proxyAdminOwner = await proxyAdmin.owner();
+            console.log('ProxyAdmin owner:', proxyAdminOwner);
+            console.log(
+                await hardhat.upgrades.erc1967.getAdminAddress(
+                    contractDeployInfo.contract.address
+                )
+            );
             const tlRequestParams = [
                 proxyAdmin.address,
                 0,
