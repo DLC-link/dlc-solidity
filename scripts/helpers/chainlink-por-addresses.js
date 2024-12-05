cec662b71eb08d325447cf942d400366c3fe2c5d
scolear <bobtera@gmail.com>
2024-12-05 17:32:19 +0100

    feat: add base por addy

--- scripts/helpers/chainlink-por-addresses.js
+++ scripts/helpers/chainlink-por-addresses.js
@@ -20,7 +20,7 @@ module.exports = async function getPoRAddress(network) {
         case 'arbsepolia':
             return '';
         case 'base':
-            return '';
+            return '0x30A76F4E688Cf52f4A06D7AAd987A7037f3Ae6f7';
         case 'basesepolia':
             return '';
         case 'localhost':
