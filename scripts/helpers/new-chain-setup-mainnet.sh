6eff735b659e741a9aa1b85326974986fecd5ea1
scolear <bobtera@gmail.com>
2024-12-05 12:46:35 +0100

    iBTC deployment

--- scripts/helpers/new-chain-setup-mainnet.sh
+++ scripts/helpers/new-chain-setup-mainnet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MEDIUM_MULTISIG=0x989a6b15c0c55C0c9FEd6E60a812d9Fb02Ad666E
+
 # Mainnet attestor public keys
 dlc-link-eth add-signer 0x989E9c4005ABc2a8E4b85544B44d2d95cfDe08de # DLC.Link
 dlc-link-eth add-signer 0xBe4aAE47A62f67bdF93eA9f5F189ae51B1b54492 # DLC.Link
@@ -18,24 +20,24 @@ dlc-link-eth set-btc-fee-recipient 3D8kYzXXsvibCqWUu7RkXZTLcx5DzmckZ2
 
 dlc-link-eth set-threshold 5
 
-# dlc-link-eth grant-role-on-manager DLC_ADMIN_ROLE $MEDIUM_MULTISIG
-
-# other todos:
-# - transfer ownership of the proxy admin contract to the DLC.Link multisig
-# - transfer ownership of the dlcBTC contract to the DLCManager (this is automated)
-# - grant DLC_ADMIN_ROLE role to the DLC.Link MEDIUM multisig
-# - renounce DLC_ADMIN_ROLE from the deployer
-# - initiate DEFAULT_ADMIN_ROLE transfer on DLCManager to the DLC.Link CRITICAL multisig
-# - Fund Coordinator with ETH for gas
-
 # - Whitelist Minters
-
 dlc-link-eth whitelist-account 0x0DD4f29E21F10cb2E485cf9bDAb9F2dD1f240Bfa # DLC.Link
 dlc-link-eth whitelist-account 0x5dd42c5fbf7f784d040c59f1720cdd8c47bbff95 # Amber
 dlc-link-eth whitelist-account 0xf92893654e38b80dfd9b4a2fb99100dd31ba5e2d # Amber
 dlc-link-eth whitelist-account 0xff200709bf9bbc5209ba4b5dd767913a8a06b73f # Amber
-dlc-link-eth whitelist-account 0x46166fA874AAEDEA8d98b15F9A72C84e22Abe2A1 # SBL has been offboarded
+dlc-link-eth whitelist-account 0x46166fA874AAEDEA8d98b15F9A72C84e22Abe2A1 # SBL is being offboarded
 dlc-link-eth whitelist-account 0x14Ee510Ebd4E5273e83Ad88f6cd2dc228BE40D12 # Tokkalabs
 dlc-link-eth whitelist-account 0xFc62319bCdfeDB6e55295F3723005593b16A3f9c # Garden
 dlc-link-eth whitelist-account 0x2816f3528AD324E6089714DA8E89455f58739e68 # Jademont
 dlc-link-eth whitelist-account 0x707A141c5c19c25E2e6D50b214e39A4293B63234 # PatternResearch
+
+# - grant DLC_ADMIN_ROLE role to the DLC.Link MEDIUM multisig
+dlc-link-eth grant-role-on-manager DLC_ADMIN_ROLE $MEDIUM_MULTISIG
+# - renounce DLC_ADMIN_ROLE from the deployer
+dlc-link-eth renounce-role-on-manager DLC_ADMIN_ROLE
+
+# other todos:
+# - transfer ownership of the proxy admin contract to the TimelockController
+# - transfer ownership of the dlcBTC contract to the DLCManager (this is automated)
+# - initiate DEFAULT_ADMIN_ROLE transfer on DLCManager to the DLC.Link CRITICAL multisig
+# - Fund Coordinator with ETH for gas
