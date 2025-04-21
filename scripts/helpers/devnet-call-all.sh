f3670ace0999163ba303a55b941a5cb25519e47d
scolear <bobtera@gmail.com>
2025-04-21 14:45:51 +0200

    remove testnet-3, add dsrc, p2p

--- scripts/helpers/devnet-call-all.sh
+++ scripts/helpers/devnet-call-all.sh
@@ -2,8 +2,8 @@
 
 # Check if the command line argument is provided
 if [ $# -eq 0 ]; then
-    echo "Please provide a command argument."
-    exit 1
+  echo "Please provide a command argument."
+  exit 1
 fi
 
 # Assign the command line argument to a variable
@@ -13,3 +13,4 @@ extra_args="${@:2}"
 HARDHAT_NETWORK=basesepolia dlc-link-eth $command $extra_args
 HARDHAT_NETWORK=arbsepolia dlc-link-eth $command $extra_args
 HARDHAT_NETWORK=sepolia dlc-link-eth $command $extra_args
+HARDHAT_NETWORK=holesky dlc-link-eth $command $extra_args
--- scripts/helpers/new-chain-setup-testnet.sh
+++ scripts/helpers/new-chain-setup-testnet.sh
@@ -6,12 +6,13 @@ export CLI_MODE='noninteractive'
 # Testnet Attestors
 dlc-link-eth add-signer 0x3355977947F84C2b1CAE7D2903a72958aEE185e2
 dlc-link-eth add-signer 0xA2c975F49f578AC3E384Ad21E2126968b82037f5
-dlc-link-eth add-signer 0x3fF31dCd8ca84b594e355CfDf913738bFc192a5c
 dlc-link-eth add-signer 0xAE9749CE4616193c7Bf8A1A395Db3C7f910120d6 # LinkPool
 dlc-link-eth add-signer 0xABD434AA01eE1A6d3E4a569B871E7e63E64F166c # HashKey
 dlc-link-eth add-signer 0x6a79E12566E01e8Fda80a15b7325478B1d68B342 # Despread
 dlc-link-eth add-signer 0xB626fa1355B4c64Ca9723D555Bd1AA9eD8491cB4 # ValidationCloud
 dlc-link-eth add-signer 0xec32836897C40dc29525db3bAfBAC18e62fab363 # Nethermind
+dlc-link-eth add-signer 0x48256F198CE3A0B9E4f145d8Aa791ECCd768a8a0 # DSRV
+dlc-link-eth add-signer 0x163269bDD306884FA4e6a71027F065604227f3c5 # P2P
 
 dlc-link-eth set-whitelisting 'false'
 
